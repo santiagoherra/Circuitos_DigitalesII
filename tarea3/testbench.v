69c4f326b862024b8e0b74ff9cc802c6b9858504
santiagoherra <santiagoherra@gmail.com>
2024-06-05 22:08:07 -0600

    ya con top

--- tarea3/testbench.v
+++ tarea3/testbench.v
@@ -1,44 +1,43 @@
-`include "master.v"
+`include "top.v"
 
-module testbench (
-    output reg clk,
-    output reg reset,
-    output reg [15:0] datain,
-    input spi_cs_l,
-    input sclk,
-    input spi_data,
-    input [15:0] counter
-);
+module tb_top;
 
-master uut (
-    .clk(clk),
-    .reset(reset),
-    .datain(datain),
-    .spi_cs_l(spi_cs_l),
-    .sclk(sclk),
-    .spi_data(spi_data),
-    .counter(counter)
-);
+    reg clk;
+    reg reset;
+    reg SLAVE_SELECT;
 
+    // Instancia del módulo top
+    top uut (
+        .clk(clk),
+        .reset(reset),
+        .SLAVE_SELECT(SLAVE_SELECT)
+    );
 
-always #5 clk = !clk;
+    // Generador de reloj
+    always #5 clk = ~clk;
 
-initial begin
-    reset = 1;
-    clk = 0;
-    datain = 0;
-    #10;
-    reset = 0;
-    #10;
-    datain = 16'hA569;
-    #335;
-    $finish;
-end
-
-initial begin
-    $dumpfile("tb.vcd");
-    $dumpvars;
-end
-
+    initial begin
+        // Inicialización
+        clk = 0;
+        reset = 1;
+        SLAVE_SELECT = 0;
+        
+        // Esperar un ciclo de reloj
+        #10;
+        
+        // Liberar reset
+        reset = 0;
+        
+        // Simulación de selección de slave
+        SLAVE_SELECT = 0; // Seleccionar slave 1
+        #10
     
+        #100;
+        SLAVE_SELECT = 1; // Seleccionar slave 2
+        #100;
+        
+        // Finalizar simulación
+        $stop;
+    end
+
 endmodule
---
+++ tarea3/top.v
@@ -0,0 +1,56 @@
+`include "master.v"
+`include "slave1.v"
+`include "slave2.v"
+
+
+module top (
+    input clk,
+    input reset,
+    input SLAVE_SELECT
+);
+
+// Interconexión de señales
+wire [15:0] MOSI;
+wire SCLK;
+wire CS_SLAVE1;
+wire CS_SLAVE2;
+wire [15:0] count;
+wire [15:0] MISO_SLAVE1;
+wire [15:0] MISO_SLAVE2;
+wire [15:0] MISO_MASTER;
+
+// Selección de MISO del master basado en cuál slave está activo
+assign MISO_MASTER = (CS_SLAVE1 == 1'b0) ? MISO_SLAVE1 : MISO_SLAVE2;
+
+// Instancia del master
+master master_inst (
+    .clk(clk),
+    .reset(reset),
+    .MISO(MISO_MASTER),
+    .SLAVE_SELECT(SLAVE_SELECT),
+    .CS_SLAVE1(CS_SLAVE1),
+    .CS_SLAVE2(CS_SLAVE2),
+    .SCLK(SCLK),
+    .MOSI(MOSI),
+    .count(count)
+);
+
+// Instancia del slave1
+slave1 slave1_inst (
+    .SCLK(SCLK),
+    .MOSI(MOSI),
+    .CS(CS_SLAVE1),
+    .count_master(count),
+    .MISO(MISO_SLAVE1)
+);
+
+// Instancia del slave2
+slave2 slave2_inst (
+    .SCLK(SCLK),
+    .MOSI(MOSI),
+    .CS(CS_SLAVE2),
+    .count_master(count),
+    .MISO(MISO_SLAVE2)
+);
+
+endmodule
